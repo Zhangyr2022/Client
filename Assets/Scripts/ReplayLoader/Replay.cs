594dda5c8bfe13787321891fbb0a4afa6d09cf8a
zhangyr <1302821779@qq.com>
2023-01-29 00:09:24 +0800

    Create the upload button
    When clicking the button, user can select the json file to watch replay

---
+++ Assets/Scripts/ReplayLoader/Replay.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+/// <summary>
+/// This script must be bound in a button
+/// </summary>
+public class Replay : MonoBehaviour
+{
+    public class ReplayInfo
+    {
+
+    }
+
+    private Upload _upload = new() { };
+    private Upload.OpenFileName _replayFile = new() { };
+    private void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(Clicked);
+    }
+    private void Clicked()
+    {
+        // Get json file
+        this._replayFile = this._upload.UploadJson();
+        // Check
+        if (this._replayFile == null)
+        {
+            return;
+        }
+
+        // Read the json file
+        JsonTextReader reader = ReadJsonFile(this._replayFile);
+        // Process the replay
+        JObject jsonObject = (JObject)JToken.ReadFrom(reader);
+        Debug.Log(jsonObject.ToString());
+    }
+    /// <summary>
+    /// Parse the json file
+    /// </summary>
+    /// <param name="fileInfo">The file from class Upload.OpenFileName</param>
+    /// <returns></returns>
+    public JsonTextReader ReadJsonFile(Upload.OpenFileName fileInfo)
+    {
+        System.IO.StreamReader file = System.IO.File.OpenText(this._replayFile.File);
+        return new JsonTextReader(file);
+    }
+
+}
---
+++ Assets/Scripts/ReplayLoader/Upload.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+/// <summary>
+/// User should upload the json replay file
+/// </summary>
+public class Upload
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public class OpenFileName
+    {
+        public int StructSize = 0;
+        public IntPtr DlgOwner = IntPtr.Zero;
+        public IntPtr Instance = IntPtr.Zero;
+        public String Filter = null;
+        public String CustomFilter = null;
+        public int MaxCustFilter = 0;
+        public int FilterIndex = 0;
+        public String File = null;
+        public int MaxFile = 0;
+        public String FileTitle = null;
+        public int MaxFileTitle = 0;
+        public String InitialDir = null;
+        public String Title = null;
+        public int Flags = 0;
+        public short FileOffset = 0;
+        public short FileExtension = 0;
+        public String DefExt = null;
+        public IntPtr CustData = IntPtr.Zero;
+        public IntPtr Hook = IntPtr.Zero;
+        public String TemplateName = null;
+        public IntPtr ReservedPtr = IntPtr.Zero;
+        public int ReservedInt = 0;
+        public int FlagsEx = 0;
+    }
+
+    /// <summary>
+    /// Open a json file 
+    /// </summary>
+    /// <returns>
+    /// OpenFileName: The file to be opened
+    /// </returns>
+    public OpenFileName UploadJson()
+    {
+        OpenFileName openFileName = new OpenFileName();
+        openFileName.StructSize = Marshal.SizeOf(openFileName);
+        openFileName.Filter = "JSON文件(*.json)\0*.json";
+        openFileName.File = new string(new char[256]);
+        openFileName.MaxFile = openFileName.File.Length;
+        openFileName.FileTitle = new string(new char[64]);
+        openFileName.MaxFileTitle = openFileName.FileTitle.Length;
+        openFileName.InitialDir = Application.streamingAssetsPath.Replace('/', '\\');//默认路径
+        openFileName.Title = "选择回放json文件";
+        openFileName.Flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000008;
+
+        if (LocalDialog.GetOpenFileName(openFileName))
+        {
+            Debug.Log(openFileName.File);
+            Debug.Log(openFileName.FileTitle);
+            return openFileName;
+        }
+        return null;
+    }
+
+    private class LocalDialog
+    {
+        //链接指定系统函数       打开文件对话框
+        [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool GetOpenFileName([In, Out] OpenFileName ofn);
+        public static bool GetOFN([In, Out] OpenFileName ofn)
+        {
+            return GetOpenFileName(ofn);
+        }
+
+        //链接指定系统函数        另存为对话框
+        [DllImport("Comdlg32.dll", SetLastError = true, ThrowOnUnmappableChar = true, CharSet = CharSet.Auto)]
+        public static extern bool GetSaveFileName([In, Out] OpenFileName ofn);
+        public static bool GetSFN([In, Out] OpenFileName ofn)
+        {
+            return GetSaveFileName(ofn);
+        }
+    }
+}
+
